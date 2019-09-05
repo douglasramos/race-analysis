0be97f01533e5f9f99352fdc55640f52440f56e0
Douglas Ramos <douglas.dcr@outlook.com>
2019-09-04 23:30:48 -0300

    Add project files.

---
+++ Presentation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Presentation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
